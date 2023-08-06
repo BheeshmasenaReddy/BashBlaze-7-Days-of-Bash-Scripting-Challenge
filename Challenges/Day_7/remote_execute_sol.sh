2789f9bc725d825e0cf90b73488611c1f11a929d
BheeshmasenaReddy <bheeshmasena99@gmail.com>
2023-08-06 22:27:40 +0530

    Added Day 7 solution

--- Challenges/Day_7/remote_execute_sol.sh
+++ Challenges/Day_7/remote_execute_sol.sh
@@ -1,2 +1,8 @@
+if [ $# -ne 1 ]; then
+    echo "Usage: ./remote_execute_sol.sh commands.sh"
+    echo "Store all the commands to be executed in servers in commands.sh"
+    exit 1
+fi
+
 commands=$1
-parallel-ssh -h hosts.txt -P -I<$1
+parallel-ssh -h hosts.txt -P -I<./$1
